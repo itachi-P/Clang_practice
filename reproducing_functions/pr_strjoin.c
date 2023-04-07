cd543f59fcf4fdb38e0cc2ff8c78845b763ec5eb
itachi-P <itachip38@gmail.com>
2023-04-07 11:48:47 +0900

    The pr_strcat function was eliminated and rewritten.

--- reproducing_functions/pr_strjoin.c
+++ reproducing_functions/pr_strjoin.c
@@ -1,54 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	pr_strlen(char *str)
+#include <stdlib.h>
+#include <string.h>
+
+/*
+** 文字列の長さを返す関数
+*/
+static int	pr_strlen(char *str)
 {
 	int	len;
 
 	len = 0;
 	while (str[len])
-		++len;
+		len++;
 	return (len);
 }
 
-char	*pr_strcat(char *dest, char *src)
-{
-	char	*ret;
-
-	ret = dest;
-	while (*dest)
-		dest++;
-	while (*src)
-		*dest++ = *src++;
-	*dest = '\0';
-	return (ret);
-}
-
+/*
+** 複数の文字列を結合する関数
+** size: 結合する文字列の個数
+** strs: 結合する文字列の配列
+** sep: 各文字列を区切るための区切り文字列
+** return: 結合された文字列へのポインタ。メモリ確保に失敗した場合はNULLを返す
+*/
 char	*pr_strjoin(int size, char **strs, char *sep)
 {
-	int		len;
-	char	*result;
-	char	*p;
+	char	*result; // 結合された文字列
+	int		result_len; // 結合された文字列の長さ
+	int		sep_len; // 区切り文字列の長さ
 
 	if (size <= 0)
 		return (NULL);
-	len = 0;
+
+	// 結合後の文字列の長さを求める
+	result_len = 0;
+	sep_len = pr_strlen(sep);
 	for (int i = 0; i < size; i++)
-		len += pr_strlen(strs[i]);
-	len += pr_strlen(sep) * (size - 1);
-	result = (char *)malloc(sizeof(char) * (len + 1));
+		result_len += pr_strlen(strs[i]) + (i < size - 1 ? sep_len : 0);
+
+	// メモリを確保して、文字列を結合する
+	result = (char *)malloc(result_len + 1);
 	if (!result)
 		return (NULL);
-	p = result;
+
+	int	idx = 0; // resultの現在のインデックス
 	for (int i = 0; i < size; i++)
 	{
+		// 区切り文字列をresultに追加
+		if (i != 0)
+			for (int j = 0; j < sep_len; j++)
+				result[idx++] = sep[j];
+
+		// 文字列をresultに追加
 		for (int j = 0; j < pr_strlen(strs[i]); j++)
-			*p++ = strs[i][j];
-		if (i != size - 1)
-			for (int j = 0; j < pr_strlen(sep); j++)
-				*p++ = sep[j];
+			result[idx++] = strs[i][j];
 	}
-	*p = '\0';
+	result[result_len] = '\0';
+
 	return (result);
 }
 
