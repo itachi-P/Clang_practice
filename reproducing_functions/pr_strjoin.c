1cc4958d8884b8f58f213af1ffcc92290e3ae2f8
itachi-P <itachip38@gmail.com>
2023-04-07 11:28:50 +0900

    Changed the internal specification of pr_strcat.

--- reproducing_functions/pr_strjoin.c
+++ reproducing_functions/pr_strjoin.c
@@ -13,39 +13,43 @@ int	pr_strlen(char *str)
 
 char	*pr_strcat(char *dest, char *src)
 {
-    while (*dest)
-        dest++;
-    while ((*dest++ = *src++))
-        ;
-    return (dest - 1);
+	char	*ret;
+
+	ret = dest;
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (ret);
 }
 
 char	*pr_strjoin(int size, char **strs, char *sep)
 {
-    int		len;
-    char	*result;
-    char	*p;
+	int		len;
+	char	*result;
+	char	*p;
 
-    if (size <= 0)
-        return (NULL);
-    len = 0;
-    for (int i = 0; i < size; i++)
-        len += pr_strlen(strs[i]);
-    len += pr_strlen(sep) * (size - 1);
-    result = (char *)malloc(sizeof(char) * (len + 1));
-    if (!result)
-        return (NULL);
-    p = result;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < pr_strlen(strs[i]); j++)
-            *p++ = strs[i][j];
-        if (i != size - 1)
-            for (int j = 0; j < pr_strlen(sep); j++)
-                *p++ = sep[j];
-    }
-    *p = '\0';
-    return (result);
+	if (size <= 0)
+		return (NULL);
+	len = 0;
+	for (int i = 0; i < size; i++)
+		len += pr_strlen(strs[i]);
+	len += pr_strlen(sep) * (size - 1);
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	p = result;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < pr_strlen(strs[i]); j++)
+			*p++ = strs[i][j];
+		if (i != size - 1)
+			for (int j = 0; j < pr_strlen(sep); j++)
+				*p++ = sep[j];
+	}
+	*p = '\0';
+	return (result);
 }
 
 int	main(void)
