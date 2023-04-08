9828a85a81a7394dbc9895f690bfd14b2a36e67b
itachi-P <itachip38@gmail.com>
2023-04-08 19:15:30 +0900

    the nostalgic FizzBuzz

---
+++ basic_practice/FizzBuzz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 31; i++)
+    {
+        if (i % 3 == 0)
+            printf("Fizz ");
+        if (i % 5 == 0)
+            printf("Buzz ");
+        if (i % 3 != 0 && i % 5 != 0)
+            printf("%d ", i);
+
+    }
+
+}
