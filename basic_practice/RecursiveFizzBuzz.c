c086a1cc31db97ef8df9df909ce491f23ad562e5
itachi-P <itachip38@gmail.com>
2023-04-09 13:21:34 +0900

    Formatted code indentation, etc.

--- basic_practice/RecursiveFizzBuzz.c
+++ basic_practice/RecursiveFizzBuzz.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-void recursive_fizzbuzz(int n)
+void	recursive_fizzbuzz(int n)
 {
-    if (n > 1)
-        recursive_fizzbuzz(n - 1);
-
-    if (n % 3 == 0 && n % 5 == 0)
-        printf("FizzBuzz! ");
-    else if (n % 3 == 0)
-        printf("Fizz ");
-    else if (n % 5 == 0)
-        printf("Buzz ");
-    else
-        printf("%d ", n);
+	if (n > 1)
+		recursive_fizzbuzz(n - 1);
+	if (n % 3 == 0 && n % 5 == 0)
+		printf("FizzBuzz! ");
+	else if (n % 3 == 0)
+		printf("Fizz ");
+	else if (n % 5 == 0)
+		printf("Buzz ");
+	else
+		printf("%d ", n);
 }
 
-int main(void)
+int	main(void)
 {
-    printf("Fizz&Buzz\n");
-    recursive_fizzbuzz(100);
+	printf("Fizz&Buzz\n");
+	recursive_fizzbuzz(100);
 }
