ff77c1cbc99013ae895457fed50027f5f51c4cc1
itachi-P <itachip38@gmail.com>
2023-04-10 12:29:12 +0900

    Added comment to Recursive_FizzBuzz

--- basic_practice/FizzBuzz.c
+++ basic_practice/FizzBuzz.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-    for (int i = 1; i <= 42; i++)
-    {
-        if (i % 3 == 0)
-            printf("Fizz ");
-        if (i % 5 == 0)
-            printf("Buzz ");
-        if (i % 3 != 0 && i % 5 != 0)
-            printf("%d ", i);
-
-    }
-
+	for (int i = 1; i <= 42; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		if (i % 5 == 0)
+			printf("Buzz ");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d ", i);
+	}
 }
--- basic_practice/RecursiveFizzBuzz.c
+++ basic_practice/RecursiveFizzBuzz.c
@@ -2,6 +2,7 @@
 
 void	recursive_fizzbuzz(int n)
 {
+	// nが1より大きい場合は、n-1から再帰的にFizzBuzzを出力する
 	if (n > 1)
 		recursive_fizzbuzz(n - 1);
 	if (n % 3 == 0 && n % 5 == 0)
@@ -18,4 +19,5 @@ int	main(void)
 {
 	printf("Fizz&Buzz\n");
 	recursive_fizzbuzz(100);
+	return (0);
 }
